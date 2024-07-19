f9cfaeb3b239d425255ba9ab54ecb19c70634997
integraweb21 <52529402+integraweb21@users.noreply.github.com>
2024-07-19 18:16:05 +0200

    Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,29 +6,25 @@ $(document).ready(function(){
     var logoHeader = $('.logo');
     var logoBanner = $('.logo-center');
 
+    logoHeader.hide();
+    logoBanner.show();
+
     iconMenu.click(function(event) {
         event.preventDefault();
 
-        // Toggle menu visibility
         menu.slideToggle();
 
-        // Show/hide logoHeader and logoBanner with fade effect
         if (logoHeader.is(':visible')) {
-            logoHeader.fadeOut();
+            logoHeader.fadeOut(function() {
+                logoBanner.fadeIn();
+            });
         } else {
-            logoHeader.fadeIn();
+            logoBanner.fadeOut(function() {
+                logoHeader.fadeIn();
+            });
         }
 
-        if (logoBanner.is(':visible')) {
-            logoBanner.fadeOut();
-        } else {
-            logoBanner.fadeIn();
-        }
-
-        // Toggle 'active' class on iconMenu, logoHeader, and logoBanner
         iconMenu.toggleClass('active');
-        logoHeader.toggleClass('active');
-        logoBanner.toggleClass('active');
 
         return false;
     });
