8ffc3350d352375b8f9dbd17bf1ad57114f08646
integraweb21 <52529402+integraweb21@users.noreply.github.com>
2024-07-22 11:25:51 +0200

    contact + projets

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
 
     // Menu
+    var header = $('header');
     var iconMenu = $('.icon-menu');
     var menu = $('.menu');
-    var logoHeader = $('header .logo');
+    var logoHeader = $('header.home .logo');
     var logoBanner = $('.logo-center');
 
     logoHeader.hide();
@@ -25,6 +26,22 @@ $(document).ready(function(){
         }
 
         iconMenu.toggleClass('active');
+
+        if (header.hasClass('page') & iconMenu.hasClass('active')) {
+            header.css({
+                position: 'fixed',
+                top: '0',
+                width: '100%',
+                height: '100%'
+            });
+        } else {
+            header.css({
+                position: '',
+                top: '',
+                width: '',
+                height: ''
+            });
+        }
 
         return false;
     });
